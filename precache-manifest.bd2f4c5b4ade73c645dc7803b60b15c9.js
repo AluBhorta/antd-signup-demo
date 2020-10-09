ff8ee127cfcf326cb62b32c9038d93ad4e160377
alu@ubuntu20-ZeDHP <dev.bhorta@gmail.com>
2020-10-09 22:17:14 +0600

    Updates

---
+++ precache-manifest.bd2f4c5b4ade73c645dc7803b60b15c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d55b198c92e30d5f0417e529a93ce07",
+    "url": "/antd-signup-demo/index.html"
+  },
+  {
+    "revision": "8356700ca5bbfb2929d5",
+    "url": "/antd-signup-demo/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "245df3f38561673a8162",
+    "url": "/antd-signup-demo/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8356700ca5bbfb2929d5",
+    "url": "/antd-signup-demo/static/js/2.98f6e92f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/antd-signup-demo/static/js/2.98f6e92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245df3f38561673a8162",
+    "url": "/antd-signup-demo/static/js/main.272b7c4b.chunk.js"
+  },
+  {
+    "revision": "e52b4a5b71667f5cc5e2",
+    "url": "/antd-signup-demo/static/js/runtime-main.575bc262.js"
+  }
+]);
